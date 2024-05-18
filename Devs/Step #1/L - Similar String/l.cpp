f969cae14abd69f09f2584a7eacb89af14c58163
Osama Ahmed <iiosamaahmed1@gmail.com>
2024-05-18 23:54:50 +0300

    solve l

--- Devs/Step #1/L - Similar String/l.cpp
+++ Devs/Step #1/L - Similar String/l.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 int main () {
   int n; cin >> n;
-  string s1, s2; cin >> s1 >> s2;
-  for (int i = 0; i < n; i++)
-  {
-    if (s1[i] != s2[i] && s1[i] != '0' && s1[i] != '1' && s2[i] != '0' && s2[i] != '1') {
-      cout << "No";
-      return 0;
+  string s1,s2; cin >> s1 >> s2;
+  if (s1 == s2) {cout << "Yes"; return 0;}
+  else {
+    for (int i = 0; i < n; i++) {
+      if (s1[i] == '0' || s1[i] == 'o' || s1[i] == '1' || s1[i] == 'l') s1[i] = 'x';
+      if (s2[i] == '0' || s2[i] == 'o' || s2[i] == '1' || s2[i] == 'l') s2[i] = 'x';
     }
-  }
-  cout << "Yes";
+    if (s1 == s2) cout << "Yes";
+    else cout << "No";
+}
 }

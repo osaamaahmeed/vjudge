32c815f3144168952a9ce1796d751d83d0936de8
Osama Ahmed <iiosamaahmed1@gmail.com>
2024-05-18 23:45:09 +0300

    solve k

--- Devs/Step #3/K - Odd Queries/k.cpp
+++ Devs/Step #3/K - Odd Queries/k.cpp
@@ -2,6 +2,33 @@
 using namespace std;
 #define ll long long
 
+bool runTests (int monthNum) {
+  if (monthNum < 10) return true;
+  else if (monthNum > 10 && monthNum%10 == monthNum/10) return true;
+  else return false;
+}
+
 int main () {
-  
+  int n; cin >> n;
+  int count = 0;
+  int arr[n];
+  for (int i = 0; i <n; i++) cin >> arr[i];
+  for (int i = 0; i < n; i++) {
+    int monthNum = i + 1;
+    // if (monthNum < 10) run = true;
+    // else if (monthNum > 10 && monthNum % 10 == monthNum / 10) run = true;
+    
+    if (runTests(monthNum)) {
+      string monthString = to_string(monthNum);
+      int res = monthNum < 10 ? monthNum : monthNum/10;
+      while (res <= arr[i]) {
+        // cout << monthNum << ": " << res << " ";
+        count++;
+        res *= 10;
+        res += monthNum < 10 ? monthNum : monthNum/10;
+      }
+      // cout << endl;
+    }
+  }
+  cout << count;
 }

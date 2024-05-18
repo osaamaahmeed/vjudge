22a3e5866f0c49e99a5142317ccb9127e6aedb6d
Osama Ahmed <iiosamaahmed1@gmail.com>
2024-05-19 01:44:38 +0300

    .

--- Devs/Step #1/I - racecar/i.cpp
+++ Devs/Step #1/I - racecar/i.cpp
@@ -3,38 +3,26 @@ using namespace std;
 #define ll long long
 
 bool pali(string s) {
-  // int reverseIndex = s.size()-1;
-  // for (int i = 0; i < s.size()/2; i++) {
-  //   if (s[i] == s[reverseIndex]) {
-  //     reverseIndex--;
-  //   } else {
-  //     return false;
-  //   }
-  // }
-  // return true;
-  int left = 0;
-  int right = s.size()-1;
-  while (left < right) {
-    if (s[left] != s[right]) {
-      return false;
-    }
-    left++;
-    right--;
-  }
-  return true;
+  string temp = s;
+  reverse(temp.begin(), temp.end());
+  if (s == temp) return true;
+  else return false;
 }
 
 int main () {
   int n; cin >> n;
-  vector<string> vec(n);
-  for (int i = 0; i < n; i++) cin >> vec[i];
-  
-  for (int i = 1; i < n; i++) {
-    string s = vec[0] + vec[i];
-    if (pali(s)) {
-      cout << "Yes";
-      return 0;
+  string arr[n];
+  for (int i = 0; i < n; i++) cin >> arr[i];
+  int reverseIndex = n-1;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (j == i) continue;
+      if (pali(arr[i]+arr[j])) {
+        // cout << arr[i] + arr[j] << " ";
+        cout << "Yes";
+        return 0;
+      }
     }
-  }
+  } 
   cout << "No";
-}
+  }
--- Devs/Step #1/J - A^A/j.cpp
+++ Devs/Step #1/J - A^A/j.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+const double E = exp(1.0);
 
 int main () {
   ll b; cin >> b;
-  for (int i = 1; i < b; i++) {
+  for (int i = 1; i <= log1pl(b); i++) {
     if (pow(i,i) == b) {
       cout << i;
       return 0;

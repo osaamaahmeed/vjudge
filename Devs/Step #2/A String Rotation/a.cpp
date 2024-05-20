ffc0873997772f5ccc10c6cf90a418c2cd6b9db9
Osama Ahmed <iiosamaahmed1@gmail.com>
2024-05-20 14:29:26 +0300

    solve

--- Devs/Step #2/A String Rotation/a.cpp
+++ Devs/Step #2/A String Rotation/a.cpp
@@ -3,24 +3,17 @@ using namespace std;
 #define ll long long
 
 int main () {
-  string s,t;
-  cin >> s >> t;
+  string s,t; cin >> s >> t;
+  string temp = s;
   if (s == t) {cout << "Yes"; return 0;}
-  else {
-    for (int i = 0; i < s.size(); i++) {
-      bool is = true;
-      for (int j = 0; j < s.size(); j++)
-      {
-        if (s[i] != t[j]) {
-          is = false;
-        } else {
-          is = true;
-          break;
-        }
-      }
-      if (is == false) {cout << "No"; return 0;}
+  for (int i = 0; i < s.size(); i++) {
+    temp = temp[temp.size()-1] + temp;
+    temp.pop_back();
+    // cout << temp << endl;
+    if (temp == t) {
+      cout << "Yes\n";
+      return 0;
     }
-    
-    // cout << "Yes";
   }
+  cout << "No";
 }
--- Devs/Step #2/B - AcCepted/b.cpp
+++ Devs/Step #2/B - AcCepted/b.cpp
@@ -2,25 +2,30 @@
 using namespace std;
 #define ll long long
 
+bool first (string s) {
+  if (s[0] == 'A') return true;
+  else return false;
+}
+
+bool small (string s) {
+  for (int i = 0; i < s.size(); i++) {
+    if (i == 0) continue;
+    if (i == 2 && s[2] == 'C') continue;
+    if (i == s.size()-2 && s[s.size()-2] == 'C') continue;
+    if (int(s[i]) > 64 && int(s[i]) < 91) return false;
+  }
+  return true;
+}
+
+bool cChar (string s) {
+  if (s[s.size()-2] == 'C' && s[2] == 'C') return false;
+  else if (s[s.size()-2] == 'C') return true;
+  else if (s[2] == 'C') return true;
+  else return false;
+}
+
 int main () {
   string s; cin >> s;
-  bool firstUpper = false;
-  bool c = false;
-  bool allLower = true;
-  if (s[0] == 'A') firstUpper = true;
-  if (s[2] == 'C' || s[s.size()-2] == 'C') {
-    c = true;
-    if (s[2] == 'C' && s[s.size()-2] == 'C') c = false;
-    for (int i = 0; i < s.size(); i++) {
-      if (i == 2 || i == s.size()-2) continue;
-      if (s[i] == 'C') {c = false; break;}
-    }
-  }
-  for (int i = 1; i < s.size(); i++)  {
-    if (i == 2 || i == s.size()-2) continue;
-    if (s[i] > 64 && s[i] < 91) {allLower = false; break;}
-  }
-  // cout << s[s.size()-2] << endl <<  firstUpper << endl << c << endl << allLower << endl;
-  if (firstUpper && c && allLower) cout << "AC";
+  if (first(s) && small(s) && cChar(s)) cout << "AC";
   else cout << "WA";
 }
